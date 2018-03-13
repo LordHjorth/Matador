f7a6a697b18a19b554410173205b4e8431d0199e
LordHjorth <hjorthen96@hotmail.com>
2018-03-13 14:55:23 +0100

    Connected!

--- Matador/src/connection/ConnectionToDB.java
+++ Matador/src/connection/ConnectionToDB.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class ConnectionToDB {
 
-	private static String dbUrl = "jdbc:mysql://localhost:3306/Matador";
+	private static String dbUrl = "jdbc:mysql://localhost/";
 	private static String dbUsername = "root";
 	private static String dbPassword = "gruppe25";
 
@@ -19,7 +19,45 @@ public class ConnectionToDB {
 		} catch (Exception e) {
 			System.err.print(e.getMessage() + " Noob - nu er du bare en meget grim og dum abe :-)");
 		}
-		return connection;
+		return null;
+	}
+
+	public void createDatabase() {
+		Connection con = dbConnect();
+		int i = 0;
+		while (i < 5) {
+			String databaseName = "Matador" + i;
+			String[] query = { "create database " + databaseName + ";", "use " + databaseName + ";",
+					"create table Spil(game_id integer primary key, name nvarchar(50), Tid_Start date, Tid_Slut date); ",
+					"create table Spiller_info(spiller_id integer, navn nvarchar(30), balance decimal, bil_farve varchar(20) unique, primary key (spiller_id), fængsel bit, chancekort nvarchar(50));",
+					"create table Bil(bil_farve varchar(20) primary key, placering integer, bil_id integer, constraint fk_Spiller_info_spiller_id foreign key (bil_id) references Spiller_info(spiller_id));",
+					"create table Ejerskab(felt_id integer, spiller_id integer, antal_huse integer, primary key (felt_id,spiller_id), constraint FK_SpillerInfo_SpillerID foreign key (spiller_id) references Spiller_info(spiller_id));",
+					"create table Spiller(spiller_id integer, game_id integer, constraint FK_Spiller_info_SpillerID foreign key(spiller_id) references Spiller_info(spiller_id), constraint FK_Spil_GameID foreign key (game_id) references Spil(game_id));" };
+			try {
+				Statement st = con.createStatement();
+				for (String q : query) {
+					st.executeUpdate(q);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			i++;
+		}
+
+	}
+
+	public void useOfStoredProcedure() {
+		Connection con = dbConnect();
+		try {
+			// Statement st = con.prepareCall("{call EmptyBandit.Ostehøvl()}");
+			CallableStatement st = con.prepareCall("call EmptyBandit.CreateNewGame(?)");
+			st.setString(1, "_hej");
+			st.execute();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- Matador/src/game/Main.java
+++ Matador/src/game/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		ConnectionToDB connect = new ConnectionToDB();
-		connect.dbConnect();
+		connect.useOfStoredProcedure();
 		SpilController spilController = new SpilController(); //opretter et objekt af SpilController
 		spilController.playGame(); //på det objekt kaldes meotden PlayGame som indeholder opsætning af gui, spillet og gameloopet.
 	} 
