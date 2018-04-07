6ca00be765c1b93a90a3e5623c0b92d4827a5c11
nicolasvahman <nicolasvahman@192.168.0.20>
2018-04-07 15:41:56 +0200

    Database-connection til min bærbar der nu er serve

--- Matador/src/connection/ConnectionToDB.java
+++ Matador/src/connection/ConnectionToDB.java
@@ -2,76 +2,68 @@ package connection;
 
 import java.sql.*;
 
+
+
 public class ConnectionToDB {
 
-	private static String dbUrl = "jdbc:mysql://localhost/EmptyBandit";
-	private static String dbUsername = "root";
-	private static String dbPassword = "gruppe25";
-	private int i = 0;
+	public static void main(String[] args) throws Exception {
 
-	public Connection dbConnect() {
-		Connection connection = null;
+		
+		createTable();
+	}
+
+	public static Connection getConnection() throws Exception {
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
-			return connection;
-		} catch (SQLException e) {
-			System.err.print(e.getMessage() + " Noob - ingen sql bandit at hente her!");
+			String driver = "com.mysql.jdbc.Driver";
+			String url = "jdbc:mysql://192.168.0.20:3306/" + "matadorspil" + "?useSSL=false";
+			
+			String username = "gruppe25";
+			String password = "25";
+			
+			Class.forName(driver);
+			
+			
+			Connection conn = DriverManager.getConnection(url, username, password);
+			
+			System.out.println("Succesfuld tilslutning");
+			return conn;
 		} catch (Exception e) {
-			System.err.print(e.getMessage() + " Noob - nu er du bare en meget grim og dum abe :-)");
+			System.out.println(e);
 		}
+
+		System.out.println("Tilslutningen er fejlet");
 		return null;
 	}
 
-//	public void createDatabase() {
-//		Connection con = dbConnect();
-//		int i = 0;
-//		while (i < 5) {
-//			String databaseName = "Matador" + i;
-//			String[] query = { "create database " + databaseName + ";", "use " + databaseName + ";",
-//					"create table Spil(game_id integer primary key, name nvarchar(50), Tid_Start date, Tid_Slut date); ",
-//					"create table Spiller_info(spiller_id integer, navn nvarchar(30), balance decimal, bil_farve varchar(20) unique, primary key (spiller_id), fængsel bit, chancekort nvarchar(50));",
-//					"create table Bil(bil_farve varchar(20) primary key, placering integer, bil_id integer, constraint fk_Spiller_info_spiller_id foreign key (bil_id) references Spiller_info(spiller_id));",
-//					"create table Ejerskab(felt_id integer, spiller_id integer, antal_huse integer, primary key (felt_id,spiller_id), constraint FK_SpillerInfo_SpillerID foreign key (spiller_id) references Spiller_info(spiller_id));",
-//					"create table Spiller(spiller_id integer, game_id integer, constraint FK_Spiller_info_SpillerID foreign key(spiller_id) references Spiller_info(spiller_id), constraint FK_Spil_GameID foreign key (game_id) references Spil(game_id));" };
-//			try {
-//				Statement st = con.createStatement();
-//				for (String q : query) {
-//					st.executeUpdate(q);
-//				}
-//			} catch (SQLException e) {
-//				e.printStackTrace();
-//			}
-//			i++;
-//		}
-//
-//	}
+	public static void createTable() throws Exception {
 
-	public void useOfStoredProcedure() {
-		Connection con = dbConnect();
 		try {
-			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("Select count(*) from EmptyBandit.Spil;");
-			if(rs.next()) {
-				if(rs.getInt(1) == 0) {
-					i = 0;
-				}
-				else {
-					ResultSet rs1 = stmt.executeQuery("Select max(id) from EmptyBandit.Spil");
-					if(rs1.next()) {
-						i = rs1.getInt(1)+1;
-					}
-				}
-			}
-			stmt.executeUpdate("INSERT INTO EmptyBandit.Spil VALUES(" + i +");");
-			CallableStatement st = con.prepareCall("call EmptyBandit.CreateNewGame(?)");
-			st.setString(1, "_" + i);
-			st.execute();
-			stmt.executeUpdate("INSERT INTO Matador_" + i + ".Spil(game_id, Tid_Start) VALUES(" + i +", now());");
-		} catch (SQLException e) {
-			e.getMessage();
+//			getConnection();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		try {
+			Connection con = getConnection();
+			PreparedStatement create = con.prepareStatement(
+					"Create Table IF NOT EXISTS Landekode (Landenavn varchar(255), Forkortelse varchar(255), Kontinent Varchar(255))");
+			create.executeUpdate();
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			System.out.println("Tabel lavet");
+		}
+
 	}
 
-}
+	
+		
+		
+		
+	}
+
+
+	
+	
+	
